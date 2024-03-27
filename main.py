c26eea16c7b804db23a4821b2c9923bf2f0c2fb2
Aleksandar Petrov <sasho1320@gmail.com>
2024-03-27 07:57:36 +0200

    update main file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db import database
 from resources.routes import api_router
+
+origins = ["http://127.0.0.1", "http://127.0.0.1:4200"]
 
 
 @asynccontextmanager
@@ -15,3 +18,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
